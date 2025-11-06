74c4ef2f2c1fabde038c32edba8006f141e24cfc
Raja <agrawalraja741@gmail.com>
2025-11-06 06:12:14 +0530

    Product MS - Exception handling changes

---
+++ ProductService/src/main/java/com/raja/ProductService/DTO/ErrorResponseDTO.java
@@ -0,0 +1,9 @@
+package com.raja.ProductService.DTO;
+
+import lombok.Data;
+
+@Data
+public class ErrorResponseDTO {
+    private String message;
+    private String status;
+}
---
+++ ProductService/src/main/java/com/raja/ProductService/controllerAdvice/ProductControllerAdvice.java
@@ -0,0 +1,35 @@
+package com.raja.ProductService.controllerAdvice;
+
+import com.raja.ProductService.DTO.ErrorResponseDTO;
+import com.raja.ProductService.Exceptions.CategoryNotFoundException;
+import com.raja.ProductService.Exceptions.ProductNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ProductControllerAdvice {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleProductNotFoundException(ProductNotFoundException e) {
+        ErrorResponseDTO exceptionDto = new ErrorResponseDTO();
+        //TODO
+        // exceptionDto.setProductId(????);
+
+        exceptionDto.setMessage(e.getMessage());
+        exceptionDto.setStatus(new String(HttpStatus.NOT_FOUND.toString()));
+        return new ResponseEntity<>(exceptionDto, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(CategoryNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleCategoryNotFoundException(CategoryNotFoundException e) {
+        ErrorResponseDTO exceptionDto = new ErrorResponseDTO();
+        //TODO
+        // exceptionDto.setProductId(????);
+
+        exceptionDto.setMessage(e.getMessage());
+        exceptionDto.setStatus(new String(HttpStatus.NOT_FOUND.toString()));
+        return new ResponseEntity<>(exceptionDto, HttpStatus.NOT_FOUND);
+    }
+}
