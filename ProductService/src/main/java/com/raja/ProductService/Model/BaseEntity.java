b7e258f497b784d37cfa4dfa9a027099142565b5
Raja <agrawalraja741@gmail.com>
2025-11-16 18:12:43 +0530

    Product MS - Auditing Changes

--- ProductService/src/main/java/com/raja/ProductService/Model/BaseEntity.java
+++ ProductService/src/main/java/com/raja/ProductService/Model/BaseEntity.java
@@ -1,18 +1,17 @@
 package com.raja.ProductService.Model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.util.Date;
 
 @Data
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity implements Serializable {
 
     @Id
@@ -20,6 +19,7 @@ public abstract class BaseEntity implements Serializable {
     private long id;
 
     @CreatedDate
+    @Column(updatable = false)
     private Date createdAt;
 
     @LastModifiedDate
